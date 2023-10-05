169289fd0e40c95031e165f239f7a87d13a39288
Barceyrim <130059831+Barceyrim@users.noreply.github.com>
2023-10-05 09:31:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom();
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+ while (contador < 9){
+
+    listaDeTeclas[0].onclick=tocaSomPom;
+ }
+
+ listaDeTeclas[1].onclick=tocaSomClip;
+ listaDeTeclas[2].onclick=tocaSomTim;
+ listaDeTeclas[3].onclick=tocaSomPuff;
+
